d3115b82ab4d855ab2b11aabb32e89e95391caca
Jonathon Artus <jonartus@gmail.com>
2019-05-06 17:59:50 +0100

    Added heartbeat.

--- src/workSpace/main.py
+++ src/workSpace/main.py
@@ -1,8 +1,15 @@
-from machine import Pin
+import ujson
+from machine import Pin, Timer
 from secret import wifi, mqtt, client_id
 from config import config
 
 led = Pin(2, Pin.OUT)
+
+heartbeat_payload = {
+  'client_id': client_id,
+  'freq': machine.freq(),
+  'heartbeat_count': 0
+}
 
 def sub_cb(topic, msg):
   print((topic, msg))
@@ -17,6 +24,9 @@ def sub_cb(topic, msg):
   if topic == b'led' and msg == b'off':
     led.value(True)
  
+def publish_heartbeat(client):
+  heartbeat_payload['heartbeat_count'] = heartbeat_payload['heartbeat_count'] + 1
+  client.publish('heartbeat', ujson.dumps(heartbeat_payload))
   
 def connect_and_subscribe():  
   client = MQTTClient(client_id, mqtt['server'], user=mqtt['user'], password=mqtt['pass'])
@@ -24,7 +34,7 @@ def connect_and_subscribe():
   client.connect()
   client.subscribe(b'led')
   client.subscribe(b'admin')
-  print('Connected to %s MQTT broker, subscribed to topics' % (mqtt_server))
+  print('Connected to %s MQTT broker, subscribed to topics' % (mqtt['server']))
   return client
 
 def restart_and_reconnect():
@@ -37,9 +47,12 @@ try:
 except OSError as e:
   restart_and_reconnect()
 
+tim = Timer(-1)
+tim.init(period=5000, mode=Timer.PERIODIC, callback=lambda _: publish_heartbeat(client))
+
 while True:
   try:
-    client.wait_msg()
+    client.check_msg()
   except OSError as e:
     restart_and_reconnect()
 
